fa876db272752503aab10bb8c7dc5e761a6d7743
kalengamarc <kayembemarc96@gmail.com>
2025-08-24 12:25:27 +0200

    teste

--- Admin/menu.php
+++ Admin/menu.php
@@ -11,7 +11,7 @@
         <li><a href="accueil.php"><i class="fas fa-home"></i> Accueil</a></li>
         <li><a href="formation.php"><i class="fas fa-folder"></i> Formations</a></li>
         <li><a href="participant.php"><i class="fas fa-users"></i> Participants</a></li>
-        <li><a href="formateur.php"><i class="fas fa-user-tie"></i> Formateurs</a></li>
+        <li><a href="formation.php"><i class="fas fa-user-tie"></i> Formateurs</a></li>
         <li><a href="equipement.php"><i class="fas fa-box"></i> Équipements</a></li>
         <li><a href="commande.php"><i class="fas fa-shopping-cart"></i> Commandes</a></li>
         <li><a href="paiement.php"><i class="fas fa-credit-card"></i> Paiements</a></li>
